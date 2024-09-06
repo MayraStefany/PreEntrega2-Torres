4ed21edb3127c4f505d75f45cfde6d5e1b1f17fa
Mayra Torres <isemayratorres18@gmail.com>
2024-09-06 00:04:01 -0500

    Test - add Layout with my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { ChakraProvider, Text } from '@chakra-ui/react';
 import React from 'react';
-import NavBar from './components/Navbar/Navbar';
+import MainLayout from "./layout/MainLayout";
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+//import NavBar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <ChakraProvider>
-      <NavBar />
+      <MainLayout>
       <ItemListContainer greeting="Bienvenidos a Popcorn & Pics!" />
+      </MainLayout>
     </ChakraProvider>
   );
 }
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+
+import NavBar from "../components/NavBar/NavBar";
+
+const MainLayout = ({children}) => {
+
+    return (
+        <div>
+            <NavBar />
+            {children}
+            <div>footer</div>
+        </div>
+    )
+};
+
+export default MainLayout;
